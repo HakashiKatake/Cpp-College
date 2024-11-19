41a2a0a61675bd645121f92bea1e724bfbdd06b5
Hakashi Katake <gammlast@gmail.com>
2024-11-19 15:21:34 +0530

    Added Operators.cpp

---
+++ Operators/operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Arithmatic
+    cout<<"Arithematic"<<endl;
+    cout<<4+5<<endl;
+    cout<<4-5<<endl;
+    cout<<4*5<<endl;
+    cout<<5/3<<endl;
+    cout<<5%3<<endl;
+
+    //Relational
+    cout<<"Relational"<<endl;
+    cout<<(4<5)<<endl;
+    cout<<(4>5)<<endl;
+    cout<<(4==5)<<endl;
+    cout<<(4<=5)<<endl;
+    cout<<(4>=5)<<endl;
+    cout<<(4!=5)<<endl;
+
+    
+}
