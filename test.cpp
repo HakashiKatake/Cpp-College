abbdf3ecb09e286e3ef07c70c445c3c86a19f230
Hakashi Katake <gammlast@gmail.com>
2024-11-19 14:26:03 +0530

    Files

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << number << endl;
+    return 0;
+}
+
