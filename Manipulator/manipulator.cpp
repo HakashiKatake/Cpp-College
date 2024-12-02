1d005bac9cffae7bd0be225d0fa2e5f73f1461fb
Hakashi Katake <gammlast@gmail.com>
2024-12-02 12:55:59 +0530

    manipulator

---
+++ Manipulator/manipulator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"Entered the two number"<<a<<b<<endl;
+    cout<<setw(5)<<a<<setw(5)<<b<<endl;
+    return 0;
+}
+//manipulators
+//setw()
+//endl
+
+//ex - setw(5)
+//ex - endl
